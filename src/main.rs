b25b2206ae9890e4bf571e64fb871fcfd7f98693
pdamianik <39028343+pdamianik@users.noreply.github.com>
2024-10-08 01:16:54 +0200

    Filter SPK Events

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,45 @@
 mod config;
 
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::LazyLock;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_web::http::header;
 use actix_web::http::header::{DispositionParam, DispositionType, QualityItem};
 use anyhow::Context;
-use icalendar::Calendar;
+use icalendar::{Calendar, CalendarComponent, Component};
+use regex::Regex;
 use tracing::{info, Level};
 use tracing_subscriber::{EnvFilter, FmtSubscriber};
 use crate::config::{AppConfig, Config};
 
 const APP_NAME: &str = "tu-planner";
 
-#[get("/")]
+const SPK_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new("\\WSPK\\W").unwrap());
+
 async fn calendar(config: web::Data<AppConfig>) -> impl Responder {
     let tiss_link = config.tiss.link();
     let response = reqwest::get(tiss_link).await.unwrap();
     let calendar = response.text().await.unwrap();
-    let calendar: Calendar = calendar.parse().unwrap();
+    let mut calendar: Calendar = calendar.parse().unwrap();
+
+    calendar.components.retain(|component| {
+        match component {
+            CalendarComponent::Event(event) => {
+                if let Some(description) = event.get_description() {
+                    !SPK_REGEX.is_match(description)
+                } else {
+                    true
+                }
+            },
+            _ => true,
+        }
+    });
+
+    let filename = "personal.ics".to_string();
     HttpResponse::Ok()
         .content_type("text/calendar")
         .insert_header(header::ContentDisposition {
             disposition: DispositionType::Attachment,
-            parameters: vec![DispositionParam::Filename("personal.ics".to_string())],
+            parameters: vec![DispositionParam::Filename(filename)],
         })
         .insert_header(header::ContentLanguage(vec![QualityItem::max(config.tiss.locale().unwrap().into())]))
         .body(format!("{calendar}"))
@@ -43,7 +61,7 @@ async fn main() -> anyhow::Result<()> {
 
     HttpServer::new(move || {
         let test = App::new()
-            .service(calendar)
+            .route("/", web::get().to(calendar))
             .app_data(web::Data::new(app_config.clone()));
         test
     })
